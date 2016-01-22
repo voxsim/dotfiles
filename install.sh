ce85da26b9b2ba99c2257987d272410e9ce46c7f
Simon Vocella <voxsim@gmail.com>
2016-01-22 14:56:31 +0100

    add tmux.conf to install.sh; pull from tpm and zsh github

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-ln -F -s $PWD/.tmux.conf ~/.tmux.conf
+# Zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  git -C ~/.oh-my-zsh pull
+fi
 
 ln -F -s $PWD/.zshrc ~/.zshrc
 if [ ! -f ~/.zshrc-personal ]; then
@@ -8,3 +13,12 @@ if [ ! -f ~/.zshrc-personal ]; then
 fi
 
 ln -F -s $PWD/voxsim.zsh-theme ~/.oh-my-zsh/themes/voxsim.zsh-theme
+
+# Tmux
+ln -F -s $PWD/.tmux.conf ~/.tmux.conf
+
+if [ ! -d ~/.tmux/plugins/tpm ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  git -C ~/.tmux/plugins/tpm pull
+fi
